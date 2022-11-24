6195b4ca1e3b142e9467c2a5728f80af6250d3b7
Aaron Pope <apope8@gmail.com>
2022-11-24 11:11:39 +0000

    Add files via upload

---
+++ Row_16/src/main/java/com/hackathon/row_16/Row16Application.java
@@ -0,0 +1,13 @@
+package com.hackathon.row_16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Row16Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Row16Application.class, args);
+    }
+
+}
---
+++ Row_16/src/test/java/com/hackathon/row_16/Row16ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.row_16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Row16ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
